33bfeaa26c65dfd1778f24ee0e6dac5aae42cae9
Морошкин Егор Владимирович <EVMoroshkin@sberbank.ru>
2022-08-08 20:22:49 +0300

    Стороны прямоугольника. [#137735]

--- src/main/java/ru/job4j/condition/SqArea.java
+++ src/main/java/ru/job4j/condition/SqArea.java
@@ -6,7 +6,6 @@ public class SqArea {
         double resultTwo = SqArea.square(4, 1);
         System.out.println(" p = 6, k = 2, s = 2, real = " + resultOne);
         System.out.println(" p = 4, k = 1, s = 1, real = " + resultTwo);
-
     }
 
     public static double square(double p, double k) {
