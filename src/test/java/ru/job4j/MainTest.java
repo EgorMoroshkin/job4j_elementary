ed0f6ca1c0925897447dc765fc04f89df41a1e4e
Морошкин Егор Владимирович <EVMoroshkin@sberbank.ru>
2022-08-08 18:46:12 +0300

    Заголовок в файле java [#159036]

--- src/test/java/ru/job4j/MainTest.java
+++ src/test/java/ru/job4j/MainTest.java
@@ -1,13 +1,13 @@
-//package ru.job4j;
-//
-//import org.junit.jupiter.api.Test;
-//import static org.assertj.core.api.Assertions.*;
-//
-//public class MainTest {
-//    @Test
-//    public void whenActionThenResult() {
-//        int excepted = 1;
-//        int result = 1;
-//        assertThat(result).isEqualTo(excepted);
-//    }
-//}
+package ru.job4j;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+    @Test
+    public void whenActionThenResult() {
+        int excepted = 1;
+        int result = 1;
+        Assertions.assertThat(result).isEqualTo(excepted);
+    }
+}
