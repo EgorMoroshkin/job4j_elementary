c81c08190f9116a2204cb7f4a05bd7e5404197d2
Морошкин Егор Владимирович <EVMoroshkin@sberbank.ru>
2022-08-11 19:05:42 +0300

    Switch [#334070 #330901]

--- src/main/java/ru/job4j/condition/SwitchWeek.java
+++ src/main/java/ru/job4j/condition/SwitchWeek.java
@@ -2,15 +2,23 @@ package ru.job4j.condition;
 
 public class SwitchWeek {
     public static String nameOfDay(int day) {
-        return switch (day) {
-            case 7 -> "Воскресенье";
-            case 6 -> "Суббота";
-            case 5 -> "Пятница";
-            case 4 -> "Четверг";
-            case 3 -> "Среда";
-            case 2 -> "Вторник";
-            case 1 -> "Понедельник";
-            default -> "Ошибка";
-        };
+        switch (day) {
+            case 7:
+                return "Воскресенье";
+            case 6:
+                return "Суббота";
+            case 5:
+                return "Пятница";
+            case 4:
+                return "Четверг";
+            case 3:
+                return "Среда";
+            case 2:
+                return "Вторник";
+            case 1:
+                return "Понедельник";
+            default:
+                return "Ошибка";
+        }
     }
 }
