f922ca212a8c6cb29e2cf02c10e39992f9e8c367
Морошкин Егор Владимирович <EVMoroshkin@sberbank.ru>
2022-08-12 08:29:09 +0300

    test

--- src/main/java/ru/job4j/condition/Test.java
+++ src/main/java/ru/job4j/condition/Test.java
@@ -2,25 +2,31 @@ package ru.job4j.condition;
 
 public class Test {
     public static void main(String[] args) {
-        int a = 1;
-        int b = 2;
-        int c = 3;
-        System.out.println(max(a, b, c));
+        int a = 2019;
+        int b = 1800;
+        int c = 2020;
+        int d = 2000;
+
+
+        System.out.println(LeapYear.checkYear(a));
+        System.out.println(LeapYear.checkYear(b));
+        System.out.println(LeapYear.checkYear(c));
+        System.out.println(LeapYear.checkYear(d));
+
 
     }
 
-    public static int max(int a, int b, int c) {
-        if (a > b) {
-            if (a > c) {
-                return a;
-            } else {
-                return c;
+    public class LeapYear {
+
+        public static boolean checkYear(int year) {
+            if (year % 4 == 0 && year % 100 != 0) {
+                return true;
+            } else if (year % 100 == 0 && year % 400 == 0) {
+                return true;
             }
+            return false;
         }
-        if (b > c) {
-            return b;
-        } else {
-            return c;
-        }
+
     }
 }
+
