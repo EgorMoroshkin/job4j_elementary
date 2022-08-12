3ed224818fa4adbe7ff6d46585c6d6f625151783
Морошкин Егор Владимирович <EVMoroshkin@sberbank.ru>
2022-08-12 16:08:52 +0300

    Math.abs [#373616 #330873]

--- src/test/java/ru/job4j/condition/ChessBoardTest.java
+++ src/test/java/ru/job4j/condition/ChessBoardTest.java
@@ -7,46 +7,46 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class ChessBoardTest {
     @Test
-    public void wayIs5() {
+    public void whenX1Is0andY1Is0andX2Is7andY2Is7ThenWayIs7() {
         int x1 = 0;
-        int y1 = 7;
-        int x2 = 0;
-        int y2 = 2;
-        int result = ChessBoard.way(x1, y1, x2, y2);
-        int expected = 5;
-        assertThat(result).isEqualTo(expected);
-    }
-
-    @Test
-    public void wayIs7() {
-        int x1 = 7;
         int y1 = 0;
-        int x2 = 0;
-        int y2 = 0;
+        int x2 = 7;
+        int y2 = 7;
         int result = ChessBoard.way(x1, y1, x2, y2);
         int expected = 7;
         assertThat(result).isEqualTo(expected);
     }
 
     @Test
-    public void wayIs0() {
-        int x1 = 2;
-        int y1 = 6;
-        int x2 = 4;
-        int y2 = 1;
+    public void whenX1Is1andY1Is1andX2Is3andY2Is3ThenWayIs2() {
+        int x1 = 1;
+        int y1 = 1;
+        int x2 = 3;
+        int y2 = 3;
         int result = ChessBoard.way(x1, y1, x2, y2);
-        int expected = 0;
+        int expected = 2;
         assertThat(result).isEqualTo(expected);
     }
 
     @Test
-    public void whenX1isMinus1ThenWayIs0() {
-        int x1 = -1;
+    public void whenX1Is6andY1Is6andX2Is3andY2Is3ThenWayIs3() {
+        int x1 = 6;
         int y1 = 6;
-        int x2 = 4;
-        int y2 = 1;
+        int x2 = 3;
+        int y2 = 3;
         int result = ChessBoard.way(x1, y1, x2, y2);
-        int expected = 0;
+        int expected = 3;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenX1Is5andY1Is5andX2Is6andY2Is6ThenWayIs1() {
+        int x1 = 5;
+        int y1 = 5;
+        int x2 = 6;
+        int y2 = 6;
+        int result = ChessBoard.way(x1, y1, x2, y2);
+        int expected = 1;
         assertThat(result).isEqualTo(expected);
     }
 
