aa749b8389afbd1681e5194012ee1e0044d05ace
Морошкин Егор Владимирович <EVMoroshkin@sberbank.ru>
2022-08-14 11:21:02 +0500

    Ипотека [#156315 #331823]

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -4,8 +4,7 @@ public class Mortgage {
     public static int year(double amount, int salary, double percent) {
         int year = 0;
         while (amount > 0) {
-            double sum = (amount * percent) / 100;
-            amount = amount + sum - salary;
+            amount = amount + (amount * percent) / 100 - salary;
             year++;
         }
         return year;
