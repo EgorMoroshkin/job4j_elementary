73bd6bebc6f6246294596d2b03643ba23acf6592
Морошкин Егор Владимирович <EVMoroshkin@sberbank.ru>
2022-08-15 07:44:14 +0500

    Создать программу, вычисляющую факториал. [#193 #331753]

--- src/main/java/ru/job4j/loop/Factorial.java
+++ src/main/java/ru/job4j/loop/Factorial.java
@@ -4,11 +4,7 @@ public class Factorial {
     public static int calc(int n) {
         int result = 1;
         for (int i = 2; i <= n; i++) {
-            if (i == 0) {
-                result = 1;
-            } else {
                 result *= i;
-            }
         }
         return result;
     }
