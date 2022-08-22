6bc30f32733dc739682d2fbe48be9d7e3c978b23
Морошкин Егор Владимирович <EVMoroshkin@sberbank.ru>
2022-08-22 20:57:27 +0300

    Жадный алгоритм. Сдача в кофе машине [#34741 #334367]

--- src/main/java/ru/job4j/array/Machine.java
+++ src/main/java/ru/job4j/array/Machine.java
@@ -8,15 +8,11 @@ public class Machine {
         int[] rsl = new int[100];
         int size = 0;
         int balance = money - price;
-        for (int i = 0; i < coins.length; i++) {
-            while (balance - coins[i] >= 0) {
-                balance = balance - coins[i];
-                if (balance < 0) {
-                    break;
-                } else {
-                    rsl[size] = coins[i];
-                    size++;
-                }
+        for (int coin : coins) {
+            while (balance - coin >= 0) {
+                balance = balance - coin;
+                rsl[size] = coin;
+                size++;
             }
         }
         return Arrays.copyOf(rsl, size);
