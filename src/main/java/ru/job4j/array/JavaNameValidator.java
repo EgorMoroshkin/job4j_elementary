2e27c1f4e7e10ca13de0c670d6e23a500ba9f991
Морошкин Егор Владимирович <EVMoroshkin@sberbank.ru>
2022-08-23 21:55:10 +0300

    String - это массив [#504884 #335390]

--- src/main/java/ru/job4j/array/JavaNameValidator.java
+++ src/main/java/ru/job4j/array/JavaNameValidator.java
@@ -5,27 +5,23 @@ public class JavaNameValidator {
         if (name.isEmpty() || Character.isUpperCase(name.codePointAt(0)) || Character.isDigit(name.codePointAt(0))) {
             return false;
         }
-        for (int i = 0; i < name.length(); i++) {
-            if (isSpecialSymbol(name.codePointAt(i))) {
-                return true;
-            } else if (isUpperLatinLetter(name.codePointAt(i))) {
-                return true;
-            } else if (isLowerLatinLetter(name.codePointAt(i))) {
-                return true;
+        for (int i = 1; i < name.length(); i++) {
+            if (isSpecialSymbol(name.codePointAt(i)) && isUpperLatinLetter(name.codePointAt(i)) && isLowerLatinLetter(name.codePointAt(i))) {
+                return false;
             }
         }
-        return false;
+        return true;
     }
 
     public static boolean isSpecialSymbol(int code) {
-        return code == 36 || code == 95;
+        return code != 36 || code != 95;
     }
 
     public static boolean isUpperLatinLetter(int code) {
-        return code >= 65 && code <= 90;
+        return code < 65 && code > 90;
     }
 
     public static boolean isLowerLatinLetter(int code) {
-        return code >= 97 && code <= 122;
+        return code < 97 && code > 122;
     }
 }
