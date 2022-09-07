8a506f56a8e411e9e694a7c4c99fd011e020fe7b
Морошкин Егор Владимирович <EVMoroshkin@sberbank.ru>
2022-09-07 20:15:06 +0300

    Рефакторинг - Расстояние между точками. [#122587]

--- src/main/java/ru/job4j/array/LoopForBoard.java
+++ src/main/java/ru/job4j/array/LoopForBoard.java
@@ -3,11 +3,11 @@ package ru.job4j.array;
 public class LoopForBoard {
     public static void main(String[] args) {
         int[][] array = new int[][]{{1, 2, 3, 4}, {5, 6, 7, 8}};
-//        for (int i = 0; i < array.length; i++) { //длинна строки
-//            for (int j = 1; j < array[i].length - 1; j++) { //длинна столбца
-//                System.out.println(array[i][j]);
-//            }
-//        }
+        for (int[] ints : array) {
+            for (int j = 1; j < ints.length - 1; j++) {
+                System.out.println(ints[j]);
+            }
+        }
         System.out.println(array.length);
     }
 }
--- src/main/java/ru/job4j/array/RandomDream.java
+++
@@ -1,9 +0,0 @@
-package ru.job4j.array;
-
-public class RandomDream {
-    public static String random(String[] prizes, int num) {
-        int len = prizes.length;
-
-        return prizes[num + 1];
-    }
-}
