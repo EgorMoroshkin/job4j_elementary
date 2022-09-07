a014238fbdbb1f0b7a915fbe127ba82dfda535de
Морошкин Егор Владимирович <EVMoroshkin@sberbank.ru>
2022-09-07 20:10:52 +0300

    Рефакторинг - Расстояние между точками. [#122587]

--- src/main/java/ru/job4j/array/Symmetry.java
+++ src/main/java/ru/job4j/array/Symmetry.java
@@ -1,14 +1,13 @@
 package ru.job4j.array;
 
 public class Symmetry {
+
     public static boolean check(int i) {
+        boolean rsl = true;
         String symm = Integer.toString(i);
         for (int j = 0; j < symm.length() / 2; j++) {
-            if (symm.charAt(j) == symm.charAt(symm.length() - 1 - j)) {
-            } else {
-                return false;
-            }
+            rsl = symm.charAt(j) == symm.charAt(symm.length() - 1 - j);
         }
-        return true;
+        return rsl;
     }
 }
